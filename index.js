5c55513f3ea9910d62458a7f56481c86e0205875
Edsel Caprice <caprice@mail.usf.edu>
2018-05-19 22:42:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians,instruments){
+  var newArray = [];
+  
+  for (i = 0; i<musicians.length;i++ ) {
+      newArray.push(musicians[i]+" plays "+instruments[i]);
+    
+  }
+  return newArray;
+}
+
